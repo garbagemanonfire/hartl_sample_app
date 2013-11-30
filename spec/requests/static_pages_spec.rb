d7c2bd15adec95a371e88a4c38a3711b323a49ae
garbagemanonfire <nilesvm@gmail.com>
2013-11-29 22:46:22 -0800

    Finisher user signup front and back end

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -52,7 +52,7 @@ describe "Static pages" do
     expect(page).to have_title(full_title('Contact'))
     click_link "Home"
     click_link "Sign up now!"
-    expect(page).to have_title(full_title('Sign Up'))
+    expect(page).to have_title(full_title('Sign up'))
     click_link "sample app"
     expect(page).to have_title(full_title(''))
   end
